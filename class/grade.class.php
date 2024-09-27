10c1e9cfde316305ecb784131e3c02909a9fe9c9
João Morais <161985742+jmorais9@users.noreply.github.com>
2024-09-27 18:19:57 -0400

    Add files via upload

---
+++ class/grade.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class GradeCurricular {
+    private $id;
+    private $descricao;
+
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getDescricao() {
+        return $this->descricao;
+    }
+
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setDescricao($descricao) {
+        $this->descricao = $descricao;
+    }
+}
+
+?>
