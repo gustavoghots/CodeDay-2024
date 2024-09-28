ea18211a77721a72c79bfd0e9acd7385e9126e09
Vinicius Michel Rosa <144750859+OvineMichel@users.noreply.github.com>
2024-09-28 15:52:57 -0300

    Create login_ok.php

---
+++ login/login_ok.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+include_once "usuario.DAO.class.php";
+include_once "usuario.class.php";
+
+$matricula = $_POST['matricula'];
+$senha = $_POST['senha'];
+
+$objUsuario = new Usuario();
+$objUsuario->setLogin($matricula);
+$objUsuario->setSenha($senha);
+
+$objUsuarioDAO = new usuarioDAO();
+$retorno = $objUsuarioDAO->login($objUsuario);
+
+if (is_array($retorno)) {
+    $_SESSION['idUsuario'] = $retorno['idusuario'];
+    header("Location: formulario.php");
+    exit(); 
+} else {
+    switch ($retorno) {
+        case 2:
+            header("Location: login.php?error=senha");
+            exit();
+        case 3:
+            header("Location: login.php?error=login");
+            exit();
+        default:
+            header("Location: login.php?error=erro");
+            exit();
+    }
+}
+?>
+
